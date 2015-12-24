e19b6a7979b1a7154bb0dad865014f250b8678f6
Patrick Lobacher <patrick@lobacher.de>
2015-12-24 08:06:35 +0100

    Persisted blogs with PersistenceManager in place

--- Classes/Controller/BlogController.php
+++ Classes/Controller/BlogController.php
@@ -3,16 +3,28 @@ namespace Pluswerk\Simpleblog\Controller;
 
 class BlogController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 {
+    /**
+     * @var \Pluswerk\Simpleblog\Domain\Repository\BlogRepository
+     */
+    protected $blogRepository;
+
+    /**
+     * @param \Pluswerk\Simpleblog\Domain\Repository\BlogRepository $blogRepository
+     */
+    public function injectBlogRepository(\Pluswerk\Simpleblog\Domain\Repository\BlogRepository $blogRepository)
+    {
+        $this->blogRepository = $blogRepository;
+    }
+
     public function listAction()
     {
-        $blogs = array();
         for ($i=1; $i<=3; $i++) {
-            /** @var \Pluswerk\Simpleblog\Domain\Model\Blog $blog */
             $blog = $this->objectManager->get('Pluswerk\\Simpleblog\\Domain\\Model\\Blog');
             $blog->setTitle('Das ist der ' . $i . '. Blog!');
-            $blogs[] = $blog;
+            $this->blogRepository->add($blog);
         }
-        $this->view->assign('blogs', $blogs);
+        $this->objectManager->get('TYPO3\\CMS\\Extbase\\Persistence\\Generic\\PersistenceManager')->persistAll();
+        $this->view->assign('blogs',$this->blogRepository->findAll());
     }
 
 }
