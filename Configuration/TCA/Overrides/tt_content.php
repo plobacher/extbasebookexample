6c988e858568cbf8976e00b642da4347d820274a
Patrick Lobacher <patrick@lobacher.de>
2015-12-28 08:38:38 +0100

    FlexForm commented

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,11 +1,11 @@
 <?php
 
 // Einbindung Flexform für Plugin Bloglisting der Extension Simpleblog
-$pluginSignature = 'simpleblog_bloglisting';
+/*$pluginSignature = 'simpleblog_bloglisting';
 
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
     $pluginSignature,
     'FILE:EXT:simpleblog/Configuration/FlexForms/FF_Simpleblog_Bloglisting.xml'
-);
+);*/
